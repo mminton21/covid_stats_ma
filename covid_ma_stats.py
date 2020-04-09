95be2551edbcae21a60ceae17836b1ec4292ac9c
Matthew Minton <minton@posteo.net>
2020-04-09 17:34:57 -0400

    used a for loop to grab both the header on the page and the figure from the page in one run instead of having to use slicing and numerous sentences

--- covid_ma_stats.py
+++ covid_ma_stats.py
@@ -5,24 +5,10 @@ URL = "https://www.mass.gov/info-details/covid-19-cases-quarantine-and-monitorin
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-#results = soup.find_all('div', class_="ma__table--responsive__wrapper")
-results = soup.find_all('td')
 
-present_confirmed_ma = results[0]
-print(present_confirmed_ma)
-
-total_individuals_quarantine = results[1]
-print(total_individuals_quarantine)
-
-total_individuals_complete_quarantine = results[2]
-print(total_individuals_complete_quarantine)
-
-total_individuals_current_quarantine = str(results[3])
-if len(total_individuals_current_quarantine) == 13:
-    ticq = total_individuals_current_quarantine[4:8]
-elif len(total_individuals_current_quarantine) == 14:
-    ticq = total_individuals_current_quarantine[4:9]
-elif len(total_individuals_current_quarantine) == 15:
-    ticq = total_individuals_current_quarantine[4:10]
-
-print("The total number of current individuals in quarantine in Massachusetts is " + ticq + ".")
+for figures in soup.find_all('tr'):
+    if figures.td is not None:
+        header = figures.th.text
+        quantity = figures.td.text
+        print(header)
+        print(quantity)
